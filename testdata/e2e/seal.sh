a4edbff9798c0b9303a87d792dff0307af43b027
Marc Brugger <github@bakito.ch>
2022-10-23 03:09:14 +0200

    Update seal.sh

--- testdata/e2e/seal.sh
+++ testdata/e2e/seal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 ./toSecret.sh ${1} \
-  | curl -s --show-error -H "Content-Type: application/json" -X POST --data-binary @- http://localhost/ssw/api/seal \
+  | curl -s --show-error -H "Content-Type: application/json" -X POST --data @- http://localhost/ssw/api/seal \
   | jq -r .secret > ${2}
