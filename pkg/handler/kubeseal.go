e7e2c5e12ceae979a2e0da8dcd19da9d6b374aed
Mihai Galos <mihai.galos@rbinternational.com>
2024-05-03 21:42:52 +0200

    feat: YAML - Multi input for sealing

--- pkg/handler/kubeseal.go
+++ pkg/handler/kubeseal.go
@@ -9,22 +9,39 @@ import (
 )
 
 func (h *Handler) KubeSeal(c *gin.Context) {
+	result := []byte{}
 	outputContentType, outputFormat, done := NegotiateFormat(c)
 	if done {
 		return
 	}
 
-	ss, err := h.sealer.Seal(outputFormat, c.Request.Body)
+	data, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
-		log.Printf("Error in %s: %v\n", Sanitize(c.Request.URL.Path), err)
-		c.Negotiate(http.StatusInternalServerError, gin.Negotiate{
-			Offered: []string{gin.MIMEJSON, gin.MIMEYAML},
-			Data:    gin.H{"error": err.Error()},
-		})
-		return
+		return err
 	}
 
-	c.Data(http.StatusOK, outputContentType, ss)
+	yamlFiles := strings.Split(string(data), "
+---
+")
+	for _, yamlFile := range yamlFiles {
+		var obj map[string]interface{}
+		if err := yaml.Unmarshal([]byte(yamlFile), &obj); err != nil {
+			return err
+		}
+
+		ss, err := h.sealer.Seal(outputFormat, obj)
+		if err != nil {
+			log.Printf("Error in %s: %v\n", Sanitize(c.Request.URL.Path), err)
+			c.Negotiate(http.StatusInternalServerError, gin.Negotiate{
+				Offered: []string{gin.MIMEJSON, gin.MIMEYAML},
+				Data:    gin.H{"error": err.Error()},
+			})
+			return
+		}
+		result = append(result, ss)
+  }
+
+	c.Data(http.StatusOK, outputContentType, result)
 }
 
 func NegotiateFormat(c *gin.Context) (string, string, bool) {
