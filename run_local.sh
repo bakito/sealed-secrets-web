d7016ed5ee60cdfbf8f78eebf4aa9a164b5f9171
bakito <github@bakito.ch>
2025-02-16 11:58:34 +0100

    update tools and local start script

--- run_local.sh
+++ run_local.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 docker start kind-registry || docker run -d --restart=always -p "127.0.0.1:5001:5000" --name kind-registry registry:2
 
 # startup kind
-if $(kind get clusters | grep -vqFx kind); then
+if ! kind get clusters | grep -qFx "kind"; then
   curl -L https://raw.githubusercontent.com/bakito/kind-with-registry-action/main/kind-config.yaml -o testdata/e2e/kind-config.yaml
   kind create cluster --name kind --config=testdata/e2e/kind-config.yaml
 fi
@@ -29,8 +29,7 @@ kubectl wait --namespace ingress-nginx \
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm upgrade --install sealed-secrets sealed-secrets/sealed-secrets \
   --namespace sealed-secrets \
-  --create-namespace \
-  --atomic
+  --create-namespace --wait
 
 # install sealed secrets web
 ./testdata/e2e/installSealedSecretsWebChart.sh yaml
