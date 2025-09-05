f3cd368e2c478f0c582066b4ca2545c479565f17
bakito <github@bakito.ch>
2025-09-05 17:32:40 +0200

    define ingress version

--- .toolbox.mk
+++ .toolbox.mk
@@ -8,6 +8,9 @@ TB_LOCALBIN ?= $(TB_LOCALDIR)/bin
 $(TB_LOCALBIN):
 	if [ ! -e $(TB_LOCALBIN) ]; then mkdir -p $(TB_LOCALBIN); fi
 
+# Helper functions
+STRIP_V = $(patsubst v%,%,$(1))
+
 ## Tool Binaries
 TB_GINKGO ?= $(TB_LOCALBIN)/ginkgo
 TB_GOLANGCI_LINT ?= $(TB_LOCALBIN)/golangci-lint
@@ -17,56 +20,58 @@ TB_MOCKGEN ?= $(TB_LOCALBIN)/mockgen
 TB_SEMVER ?= $(TB_LOCALBIN)/semver
 
 ## Tool Versions
-TB_GINKGO_VERSION ?= v2.24.0
+TB_GINKGO_VERSION ?= v2.25.3
+TB_GINKGO_VERSION_NUM ?= $(call STRIP_V,$(TB_GINKGO_VERSION))
 TB_GOLANGCI_LINT_VERSION ?= v2.4.0
-TB_GORELEASER_VERSION ?= v2.11.2
+TB_GOLANGCI_LINT_VERSION_NUM ?= $(call STRIP_V,$(TB_GOLANGCI_LINT_VERSION))
+TB_GORELEASER_VERSION ?= v2.12.0
 TB_HELM_DOCS_VERSION ?= v1.14.2
 TB_MOCKGEN_VERSION ?= v0.6.0
-TB_SEMVER_VERSION ?= v1.1.5
+TB_SEMVER_VERSION ?= v1.1.7
 
 ## Tool Installer
 .PHONY: tb.ginkgo
-tb.ginkgo: $(TB_GINKGO) ## Download ginkgo locally if necessary.
-$(TB_GINKGO): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/ginkgo || GOBIN=$(TB_LOCALBIN) go install github.com/onsi/ginkgo/v2/ginkgo@$(TB_GINKGO_VERSION)
+tb.ginkgo: ## Download ginkgo locally if necessary.
+	@test -s $(TB_GINKGO) && $(TB_GINKGO) --version | grep -q $(TB_GINKGO_VERSION_NUM) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/onsi/ginkgo/v2/ginkgo@$(TB_GINKGO_VERSION)
 .PHONY: tb.golangci-lint
-tb.golangci-lint: $(TB_GOLANGCI_LINT) ## Download golangci-lint locally if necessary.
-$(TB_GOLANGCI_LINT): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/golangci-lint || GOBIN=$(TB_LOCALBIN) go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@$(TB_GOLANGCI_LINT_VERSION)
+tb.golangci-lint: ## Download golangci-lint locally if necessary.
+	@test -s $(TB_GOLANGCI_LINT) && $(TB_GOLANGCI_LINT) --version | grep -q $(TB_GOLANGCI_LINT_VERSION_NUM) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@$(TB_GOLANGCI_LINT_VERSION)
 .PHONY: tb.goreleaser
-tb.goreleaser: $(TB_GORELEASER) ## Download goreleaser locally if necessary.
-$(TB_GORELEASER): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/goreleaser || GOBIN=$(TB_LOCALBIN) go install github.com/goreleaser/goreleaser/v2@$(TB_GORELEASER_VERSION)
+tb.goreleaser: ## Download goreleaser locally if necessary.
+	@test -s $(TB_GORELEASER) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/goreleaser/goreleaser/v2@$(TB_GORELEASER_VERSION)
 .PHONY: tb.helm-docs
-tb.helm-docs: $(TB_HELM_DOCS) ## Download helm-docs locally if necessary.
-$(TB_HELM_DOCS): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/helm-docs || GOBIN=$(TB_LOCALBIN) go install github.com/norwoodj/helm-docs/cmd/helm-docs@$(TB_HELM_DOCS_VERSION)
+tb.helm-docs: ## Download helm-docs locally if necessary.
+	@test -s $(TB_HELM_DOCS) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/norwoodj/helm-docs/cmd/helm-docs@$(TB_HELM_DOCS_VERSION)
 .PHONY: tb.mockgen
-tb.mockgen: $(TB_MOCKGEN) ## Download mockgen locally if necessary.
-$(TB_MOCKGEN): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/mockgen || GOBIN=$(TB_LOCALBIN) go install go.uber.org/mock/mockgen@$(TB_MOCKGEN_VERSION)
+tb.mockgen: ## Download mockgen locally if necessary.
+	@test -s $(TB_MOCKGEN) || \
+		GOBIN=$(TB_LOCALBIN) go install go.uber.org/mock/mockgen@$(TB_MOCKGEN_VERSION)
 .PHONY: tb.semver
-tb.semver: $(TB_SEMVER) ## Download semver locally if necessary.
-$(TB_SEMVER): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/semver || GOBIN=$(TB_LOCALBIN) go install github.com/bakito/semver@$(TB_SEMVER_VERSION)
+tb.semver: ## Download semver locally if necessary.
+	@test -s $(TB_SEMVER) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/bakito/semver@$(TB_SEMVER_VERSION)
 
 ## Reset Tools
 .PHONY: tb.reset
 tb.reset:
 	@rm -f \
-		$(TB_LOCALBIN)/ginkgo \
-		$(TB_LOCALBIN)/golangci-lint \
-		$(TB_LOCALBIN)/goreleaser \
-		$(TB_LOCALBIN)/helm-docs \
-		$(TB_LOCALBIN)/mockgen \
-		$(TB_LOCALBIN)/semver
+		$(TB_GINKGO) \
+		$(TB_GOLANGCI_LINT) \
+		$(TB_GORELEASER) \
+		$(TB_HELM_DOCS) \
+		$(TB_MOCKGEN) \
+		$(TB_SEMVER)
 
 ## Update Tools
 .PHONY: tb.update
 tb.update: tb.reset
 	toolbox makefile -f $(TB_LOCALDIR)/Makefile \
-		github.com/onsi/ginkgo/v2/ginkgo \
-		github.com/golangci/golangci-lint/v2/cmd/golangci-lint \
+		github.com/onsi/ginkgo/v2/ginkgo?--version \
+		github.com/golangci/golangci-lint/v2/cmd/golangci-lint?--version \
 		github.com/goreleaser/goreleaser/v2 \
 		github.com/norwoodj/helm-docs/cmd/helm-docs \
 		go.uber.org/mock/mockgen@github.com/uber-go/mock \
