19f20f5565839521ec7e09d1008bd1c63d04f951
bakito <github@bakito.ch>
2026-03-03 22:24:08 +0100

    to

--- testdata/e2e/installSealedSecretsWebChart.sh
+++ testdata/e2e/installSealedSecretsWebChart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 
 helm upgrade --install sealed-secrets-web chart \
   --namespace sealed-secrets-web \
@@ -7,16 +7,18 @@ helm upgrade --install sealed-secrets-web chart \
   -f testdata/e2e/e2e-values.yaml \
   --set format=${1} \
   --set sealedSecrets.certURL=${2} \
-  --wait=watcher
+  --wait=watcher \
+  --timeout=1m
 
 echo "Wait for service to respond"
-timeout 30s bash <<EOT
+timeout 300s bash <<EOT
 while true; do
   if [[ "\$(curl -s http://localhost/ssw/_health)" == "OK" ]]; then
     echo "Service Running"
     break
   fi
-  sleep 1
+  sleep 10
   echo -n "."
+  kubectl get svc  -n sealed-secrets-web
 done
 EOT
