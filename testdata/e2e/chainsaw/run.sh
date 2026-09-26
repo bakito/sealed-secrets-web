47e3203b90ff3d4d916a1f8af1f2333a6c2f3123
bakito <github@bakito.ch>
2026-09-26 11:15:29 +0200

    test: use correct image in e2e test
    Signed-off-by: bakito <github@bakito.ch>

--- testdata/e2e/chainsaw/run.sh
+++ testdata/e2e/chainsaw/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-helm template cs-test-1 chart  -n '($namespace)' > ./testdata/e2e/chainsaw/template-default.yaml
-helm template cs-test-2 chart --set includeLocalNamespaceOnly=true -n '($namespace)' > ./testdata/e2e/chainsaw/template-local-ns.yaml
-helm template cs-test-3 chart --set sealedSecrets.serviceName=  -n '($namespace)' > ./testdata/e2e/chainsaw/template-service-name.yaml
-helm template cs-test-4 chart --set disableLoadSecrets=true -n '($namespace)' > ./testdata/e2e/chainsaw/template-disable-load-secrets.yaml
-helm template cs-test-5 chart --set disableLoadSecrets=false -n '($namespace)' > ./testdata/e2e/chainsaw/template-enable-load-secrets.yaml
+IMAGE_REPO="${IMAGE_REPOSITORY:-sealed-secrets-web}"
+IMAGE_TAG="${IMAGE_TAG:-e2e}"
+
+helm template cs-test-1 chart --set image.repository="${IMAGE_REPO}" --set image.tag="${IMAGE_TAG}" -n '($namespace)' > ./testdata/e2e/chainsaw/template-default.yaml
+helm template cs-test-2 chart --set image.repository="${IMAGE_REPO}" --set image.tag="${IMAGE_TAG}" --set includeLocalNamespaceOnly=true -n '($namespace)' > ./testdata/e2e/chainsaw/template-local-ns.yaml
+helm template cs-test-3 chart --set image.repository="${IMAGE_REPO}" --set image.tag="${IMAGE_TAG}" --set sealedSecrets.serviceName=  -n '($namespace)' > ./testdata/e2e/chainsaw/template-service-name.yaml
+helm template cs-test-4 chart --set image.repository="${IMAGE_REPO}" --set image.tag="${IMAGE_TAG}" --set disableLoadSecrets=true -n '($namespace)' > ./testdata/e2e/chainsaw/template-disable-load-secrets.yaml
+helm template cs-test-5 chart --set image.repository="${IMAGE_REPO}" --set image.tag="${IMAGE_TAG}" --set disableLoadSecrets=false -n '($namespace)' > ./testdata/e2e/chainsaw/template-enable-load-secrets.yaml
 
 chainsaw test --test-dir ./testdata/e2e/chainsaw/
